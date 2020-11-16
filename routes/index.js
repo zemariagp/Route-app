49bca1bbd5248bac1512dc96fda41283de3be24e
djnokin <nokindj@gmail.com>
2020-11-16 16:25:25 +0000

    done

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,13 @@ router.get("/zomato", (req, res) => {
 })
 
 router.get("/restaurant-search", (req, res) => {
-  res.render("restaurants-result");
+  const markers = [
+    { lat: 38.7129146, lng: -9.1286218 },
+    { lat: 38.7117206, lng: -9.1264315 },
+    { lat: 38.7123872, lng: -9.1287935}
+  ];
+  let markersString = JSON.stringify(markers);
+  res.render("restaurants-result", {markers: markersString});
 })
 
 router.get("/zomato-results", (req, res) => {
