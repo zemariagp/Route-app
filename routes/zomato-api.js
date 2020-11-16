6225b2a1775b83cd15109b715bf91363f40365de
djnokin <nokindj@gmail.com>
2020-11-16 15:43:52 +0000

    done

---
+++ routes/zomato-api.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+
+
+const zomatoAPI = axios.create({
+  baseURL: 'https://developers.zomato.com/api/v2.1/',
+  headers: { 'user-key': process.env.ZOMATO_KEY }
+});
+
+
+
+
+/*
+zomatoAPI.get('/categories', async (req, res) => {
+  const data = await req.data
+  console.log(data)
+})
+router.get('/categories', async (req, res) => {
+  const data = await zomatoAPI.get('/categories')
+  const categories = data.data.categories
+  console.log(categories)
+  //res.render('dashboard',{ categories })
+})
+*/
+
+module.exports = router;
+
+
