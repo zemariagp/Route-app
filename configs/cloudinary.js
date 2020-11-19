6fa257cb9445f00ad392de9dcab87cf57c3ba679
Sara Wilcox <saraewilcox@gmail.com>
2020-11-19 18:27:27 +0000

    done

--- configs/cloudinary.js
+++ configs/cloudinary.js
@@ -1,9 +1,8 @@
 const cloudinary = require('cloudinary').v2; // methods to connect to cloudinary cloud
-
 const { CloudinaryStorage } = require('multer-storage-cloudinary'); //methods to deal with form data requests
-
 const multer = require('multer'); //methods to connect multer with cloudinary
 
+//when in doubt how to fill this out look at the manual! uppercase is a convention. comes from the environment file.
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
@@ -14,7 +13,7 @@ cloudinary.config({
 const storage=new CloudinaryStorage({
   cloudinary,
   params: {
-  folder: 'streetart', //????????
+  folder: 'streetart',
   allowedFormats: ['jpg', 'png'], 
   },
   filename: function(req, file, cb) {
--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,27 @@ const Streetart = require('../models/Streetart')
 const express = require('express');
 const router  = express.Router();
 const app = require('../app');
+const fileUpload = require('../configs/cloudinary')
 
 router.get('/api/all', (req, res) => {
   Streetart.find().then(resultsDB => {
     let results = [];
     resultsDB.forEach((result) => {
-      results.push({lat: result.latitude, lng: result.longitude, address:result.address,artist:result.artist, name_description:result.name_description, imageUrl:result.imageUrl, comment:result.comment})
+      results.push({lat: result.latitude, lng: result.longitude, address:result.address,artist:result.artist, name_description:result.name_description, imageUrl:result.imageUrl, comment:result.comment, id: result._id})
     })
     res.json({results});
+  })
+})
+
+router.post('/streetart/create', fileUpload.single('image'), (req, res) => {
+  const { title, description } = req.body;
+  let fileUrlOnCloudinary = req.file.path;
+  Streetart.create({
+    title,
+    description,
+    imageUrl: fileUrlOnCloudinary
+  }).then(() => {
+    res.redirect('/streetart');
   })
 })
 
